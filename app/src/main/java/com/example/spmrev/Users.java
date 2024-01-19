cbd1fb5ca9dab05fa5cd12e45d91e1d78371ae24
UserMountain <abdmunim1@gmail.com>
2024-01-19 17:04:26 +0800

    This is test

--- app/src/main/java/com/example/spmrev/Users.java
+++ app/src/main/java/com/example/spmrev/Users.java
@@ -1,17 +1,21 @@
 package com.example.spmrev;
 
 public class Users {
-    String firstName, lastName, age, userName;
+    String firstName,firstName1, lastName, age, userName;
     public Users() {
     }
-    public Users(String firstName, String lastName, String age, String userName) {
+    public Users(String firstName,String firstName1, String lastName, String age, String userName) {
         this.firstName = firstName;
+        this.firstName1 = firstName1;
         this.lastName = lastName;
         this.age = age;
         this.userName = userName;
     }
     public String getFirstName() {
         return firstName;
+    }
+    public String getFirstName1() {
+        return firstName1;
     }
     public void setFirstName(String firstName) {
         this.firstName = firstName;
